220f0999ecbd1b68f89a86656ad49459303df059
Ankita Gosain <mailankitagosain@gmail.com>
2023-10-08 21:03:53 -0400

    adding practice labs

---
+++ PracticeLabs/2ndOctLab.py
@@ -0,0 +1,10 @@
+# Task Day1(2 Oct)
+
+# Print Hello World
+print("Hello World")
+
+# Print the calculation of 23+45
+print(23+45)
+
+# Print the calculation of 23*45
+print(23*45)
---
+++ PracticeLabs/4thOctLab.py
@@ -0,0 +1,22 @@
+"""
+Task 4th Oct 2023
+
+
+Print the Name of the user by taking it from the input command.
+Take first name and last name of user and print with sep. = "-" and end with /t
+Take a user input as name and say with message
+You are welcome. "<Pramod>" to the Python Class.
+
+"""
+UserName = input('Enter your name:')
+print(UserName)
+
+
+firstName = input('Enter first name:')
+lastName =input('Enter last name:')
+print(firstName, lastName, sep='-', end='/t')
+
+
+Name= input("\n Enter your name")
+print('You are welcome',Name,'to the Python Class.')
+
