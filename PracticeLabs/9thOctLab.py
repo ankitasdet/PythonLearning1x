871d41c8847395da69bfc5e96083f76362686b33
Ankita Gosain <mailankitagosain@gmail.com>
2023-10-09 23:29:52 -0400

    practicelab

---
+++ PracticeLabs/9thOctLab.py
@@ -0,0 +1,12 @@
+#  Create a program ,take 5 numbers from the user
+#  add 1-2 duplicates and then print the non-duplicate numbers
+
+print('Please enter any five numbers\n')
+n1 = int(input('Enter first number\n'))
+n2 = int(input('Enter second number\n'))
+n3 = int(input('Enter third number\n'))
+n4 = int(input('Enter fourth number\n'))
+n5 = int(input('Enter fifth number\n'))
+
+set={n1,n2,n3,n4,n5}
+print (set)
